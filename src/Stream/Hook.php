8dc7d3a1a6a207c16201c0cf4be5865717a55379
Gustavo Vilas Boas <gustavo.boas@startaideia.com.br>
2021-08-15 18:03:42 -0300

    chore: add after and before hook properties

--- src/Stream/Hook.php
+++ src/Stream/Hook.php
@@ -5,6 +5,20 @@ namespace Pipes\Stream;
 abstract class Hook
 {
     /**
+     * Indicates actions that run before this hook
+     * 
+     * @var array
+     */
+    protected $before = [];
+
+    /**
+     * Indicates actions that run after this hook
+     * 
+     * @var array
+     */
+    protected $after = [];
+
+    /**
      * Handle hook dispatch
      *
      * @param mixed $payload
