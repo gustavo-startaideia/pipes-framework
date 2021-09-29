097f6d3e28715d53996be94a60d17ef54b8e2790
Gustavo Vilas Boas <gu.boas13@gmail.com>
2021-09-29 19:17:20 -0300

    chore: update doc comments

--- src/Stream/Contracts/StreamConnectorContract.php
+++ src/Stream/Contracts/StreamConnectorContract.php
@@ -7,7 +7,19 @@ use Pipes\Stream\Hook;
 
 interface StreamConnectorContract
 {
+    /**
+     * Dispatches an action into the stream
+     * 
+     * @param Action $action
+     * @return mixed
+     */
     public function dispatch(Action $action): mixed;
 
+    /**
+     * Push a hook into the stream container
+     * 
+     * @param Hook $hook
+     * @return mixed
+     */
     public function pushHook(Hook $hook): StreamConnectorContract;
 }
